5de84069196fb975128a6bd886612461fc92e491
GUILHERME HALTER NUNES <guilherme_h_nunes@estudante.sesisenai.org.br>
2025-11-06 15:14:02 -0300

    Feat: Mudando linguagem do Backend

---
+++ backend/OtimizeTour/src/test/java/com/example/OtimizeTour/OtimizeTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OtimizeTour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtimizeTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- backend/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UsuarioController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
